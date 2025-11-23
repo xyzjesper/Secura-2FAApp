6d825360044604a8a092326eef616d4f5497eb27
xyzjesper <dm@xyzjesper>
2025-11-23 19:27:53 +0100

    bump versions

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
       manifestPlaceholders["usesCleartextTraffic"] = "false"
       applicationId = "dev.xyzjesper.fa_app"
       minSdk = 24
-      targetSdk = 34
+      targetSdk = 36
       versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
       versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
     }
